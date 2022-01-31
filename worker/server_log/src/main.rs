cf57b2dcfd603b2b0472e04ffe2aaf2a46be95e1
Elison Crum <elisoncrum@gmail.com>
2022-01-31 07:36:39 -0800

    Update main.rs

--- worker/server_log/src/main.rs
+++ worker/server_log/src/main.rs
@@ -3,7 +3,7 @@
 
 extern crate chrono;
 use self::chrono::{DateTime, Utc};
-use sqlx::mysql::MySqlPoolOptions;
+use sqlx::mysql::{MySqlPoolOptions, MySqlRow};
 use sqlx::{FromRow, Row};
 
 #[derive(Debug, FromRow)]
@@ -17,13 +17,13 @@ struct Server {
     clicks: i32,
     ipaddress: String,
     response_time: i32,
-    last_checked: chrono::DateTime<chrono::Utc>
+    last_checked: chrono::NaiveDateTime,
 }
 
 #[derive(Debug, FromRow)]
 struct ServerLog {
     id: i32,
-    datetime: chrono::DateTime<chrono::Utc>,
+    datetime: chrono::NaiveDateTime,
     server_id: i32,
     response_code: i32,
     response_time: i32,
@@ -40,5 +40,25 @@ async fn main() -> Result<(), sqlx::Error> {
 		.connect("mysql://testing:testing@localhost/testing")
         .await?;
 
+    let select_query = sqlx::query("SELECT * FROM server");
+
+    let servers: Vec<Server> = select_query
+        .map(|row: MySqlRow| Server {
+            id: row.get("id"),
+            domain_name: row.get("domain_name"),
+            domain_type: row.get("domain_type"),
+            agency: row.get("agency"),
+            organization: row.get("organization"),
+            status: row.get("status"),
+            clicks: row.get("clicks"),
+            ipaddress: row.get("ipaddress"),
+            response_time: row.get("response_time"),
+            last_checked: row.get("last_checked")
+            
+        })
+        .fetch_all(&pool)
+        .await?;
+    
+        println!("\n=== select tickets with query.map...:\n{:?}", servers);
     Ok(())
 }
