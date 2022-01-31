57030aeee9fbb0899c8ba4b0dfaf564de7d5218b
Elison Crum <elisoncrum@gmail.com>
2022-01-31 07:14:24 -0800

    Update Cargo and Main.rs

--- worker/server_log/src/main.rs
+++ worker/server_log/src/main.rs
@@ -1,12 +1,44 @@
 
+#![allow(unused)]
+
+extern crate chrono;
+use self::chrono::{DateTime, Utc};
 use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{FromRow, Row};
+
+#[derive(Debug, FromRow)]
+struct Server {
+    id: i32,
+    domain_name: String,
+    domain_type: String,
+    agency: i32,
+    organization: String,
+    status: String,
+    clicks: i32,
+    ipaddress: String,
+    response_time: i32,
+    last_checked: chrono::DateTime<chrono::Utc>
+}
+
+#[derive(Debug, FromRow)]
+struct ServerLog {
+    id: i32,
+    datetime: chrono::DateTime<chrono::Utc>,
+    server_id: i32,
+    response_code: i32,
+    response_time: i32,
+    ipaddress: String,
+    url: String,
+    error: String,
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-	// 1) Create a connection pool
 	let pool = MySqlPoolOptions::new()
 		.max_connections(5)
-		.connect("postgres://postgres:welcome@localhost/postgres");
+		.connect("mysql://testing:testing@localhost/testing")
+        .await?;
 
-        Ok(())
+    Ok(())
 }
