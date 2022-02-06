58b5ecc38941e9474a6fb2e12ec822e83f130edb
Elison Crum <elisoncrum@gmail.com>
2022-02-05 19:11:34 -0800

    Database Tools New

--- database-tools.py
+++ database-tools.py
@@ -5,6 +5,7 @@ from app import config
 from sqlmodel import Session
 from sqlmodel import create_engine
 from app.models.server import Server
+from sqlmodel import select
 
 API_ENVIRONMENT = os.environ.get("API_ENVIRONMENT", "Testing")
 settings = getattr(sys.modules[__name__].config, API_ENVIRONMENT)
@@ -30,6 +31,41 @@ def get_settings():
 def import_group():
     pass
 
+@import_group.command(name="catagories")
+def import_catagories():
+    """
+    Commands for importing a database.
+    """
+
+    import yaml
+    from app.models.server import Catagory, ServerCatagoryLink
+    print("Looking for catagories.yml")
+
+    with Session(engine) as session:
+        with open("config.yml", "r") as stream:
+            catagories = yaml.safe_load(stream)
+
+            for name, data in catagories.get("catagories").items():
+                print(data)
+                _catagory = Catagory(
+                    title = name,
+                    meta_ref = name.lower().replace(" ", "-"),
+                    color = f"#{data['color']}"
+                )
+
+                session.add(_catagory)
+                session.commit()
+                session.refresh(_catagory)
+
+                _query = select(Server).where(Server.domain_name.like(data['match']))
+                _result = session.exec(_query).all()
+                for server in _result:
+                    _link = ServerCatagoryLink(
+                        server_id = server.id,
+                        catagory_id = _catagory.id
+                    )
+                    session.add(_link)
+                session.commit()
 
 @import_group.command(name="csv")
 def csv_file(file):
